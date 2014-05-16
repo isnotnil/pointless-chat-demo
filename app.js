070f5b54d4f100130c32ac79778ce9565bf91c3d
Paul R. Bahler <paul@isnotnil.net>
2014-05-15 20:21:00 -0400

    Hello world via express

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-console.log('Hello World!');
+var express = require('express');
 
+var app = express();
+
+app.get('/', function (request, response) {
+    response.send('Hello, World!');
+});
+
+var server = app.listen(4000, function() {
+    console.log('Server listening on port %d', server.address().port);
+});
